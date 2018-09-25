a93ee88dcd0021b1af9580ff6c44419616201c05
underflowed <40325724+underflowed@users.noreply.github.com>
2018-09-24 23:09:40 -0400

    streamlined input

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,36 @@
 //Text-adventure game in rust
 use std::io::stdin;
 
-fn main() {
+fn get_input() -> String{
     let mut input = String::new();
+    stdin().read_line(&mut input).expect("Didn't enter string!");
+    input
+}
 
-    while input.contains("shovel") != true {
-        println!(
-            "You are stuck in a room with no reasonable way out, there's a shovel next to you
+fn main() {
+    println!(
+        "You are stuck in a room with no reasonable way out, there's a shovel next to you
         and the walls look like dirt"
-        );
-        stdin().read_line(&mut input).expect("Didn't enter string!");
+    );
+    let mut input = String::new();
+    while input.contains("shovel") != true {
+        input = get_input();
+        if input.contains("shovel") != true {
+            println!("your character doesn't know what that is");
+        }
     }
     room2();
 }
 
+
 fn room2() {
+    println!("You dug yourself out, congrats, you see a door in front of you");
     let mut input = String::new();
     while input.contains("door") != true {
-        println!("You dug yourself out, congrats, you see a door in front of you");
-        stdin().read_line(&mut input).expect("Didn't enter string!");
+        input = get_input();
+        if input.contains("door") || input.contains("memes") != true{
+            println!("your character doesn't know what that is");
+        }
         if input.contains("memes") {
             println!("the door has no memes to repart");
         }
@@ -27,11 +39,17 @@ fn room2() {
 }
 
 fn room3() {
+    println!("You creak open the door, and in front of you is an axe!");
     let mut input = String::new();
     let mut inventory = vec!["Shovel"];
-    while input.contains("axe") != true {
-        println!("You creak open the door, and in front of you is an axe!");
-        stdin().read_line(&mut input).expect("Didn't enter string!");
+    while input.contains("axe") & input.contains("grab") != true {
+        input = get_input();
+        if input.contains("lick") & input.contains("") != true{
+            println!("your character doesn't know what that is");
+        }
+        if input.contains("lick") & input.contains("axe") {
+            println!("bunny what the fuck");
+        }
     }
     println!("You put the axe in your inventory");
     inventory.push("Axe");
