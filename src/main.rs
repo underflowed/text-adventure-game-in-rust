6387a613c53d758675d7f7618b7c7048d4b2883f
underflowed <40325724+underflowed@users.noreply.github.com>
2018-09-26 14:42:15 -0400

    completely changed the system, prob made it worse OMEGALUL

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,90 @@
 //Text-adventure game in rust
 use std::io::stdin;
 
-fn get_input() -> String{
+// Decleration of any structs the game will use
+struct Player {
+    Inventory: Vec<String>,
+    Amount_of_things_licked: i32,
+}
+struct Room {
+    Description: String,
+}
+
+fn get_input() -> String {
     let mut input = String::new();
     stdin().read_line(&mut input).expect("Didn't enter string!");
     input
 }
 
 fn main() {
-    println!(
-        "You are stuck in a room with no reasonable way out, there's a shovel next to you
-        and the walls look like dirt"
-    );
-    let mut input = String::new();
-    while !(input.contains("shovel")) {
-        input = get_input();
-        if !(input.contains("shovel")) {
-            println!("your character doesn't know what that is");
-        }
-    }
-    room2();
+    //this is prob useless ill fix this
+    GameLoop();
 }
 
+fn GameLoop() {
+    //Player Properties
+    let mut player = Player {
+        Inventory: vec!["Shovel".to_string()],
+        Amount_of_things_licked: 0,
+    };
+    //Room object declarations
+    let room1 = Room{
+        Description : String::from("You wake up in what looks like a room, dirt lines the walls, the only source of light is a torch,
+        and there's a shovel in your hand.")
+    };
+    let room2 = Room {
+        Description: String::from(
+            "In the room you see a door to the left wall, As well as an axe in front of you",
+        ),
+    };
+    let room3 = Room {
+        Description: String::from(
+            "The room is filled with bodies, not fresh, however they look like they've
+        been here a while. A door",
+        ),
+    };
 
-fn room2() {
-    println!("You dug yourself out, congrats, you see a door in front of you");
     let mut input = String::new();
-    while !(input.contains("door")) {
-        input = get_input();
-        if !(input.contains("door") || input.contains("memes")){
-            println!("your character doesn't know what that is");
-        }
-        if input.contains("memes") {
-            println!("the door has no memes to repart");
+    //Counting amount of things licked, will lead to secret ending if lick counter reaches 50.
+    if input.contains("lick") {
+        println!("bunny stop");
+        player.Amount_of_things_licked += 1
+    }
+    if input.contains("inventory"){
+        println!("You currently have");
+        for i in 0..player.Inventory.len() {
+            println!("{}", player.Inventory[i]);
         }
     }
-    room3();
-}
 
-fn room3() {
-    println!("You creak open the door, and in front of you is an axe!");
-    let mut input = String::new();
-    let mut inventory = vec!["Shovel"];
-    while !(input.contains("grab") && input.contains("axe")) {
-        input = get_input();
-        if !(input.contains("lick") || input.contains("axe")) {
-            println!("your character doesn't know what that is");
+    //Currently treating each room as a seperate loop please fix if you can this just feels wrong LOL
+    loop {
+        println!("{}", room1.Description);
+        if input.is_empty() == true {
+            input = get_input();
         }
-        if input.contains("lick"){
-            println!("bunny what the fuck");
+        if input.contains("shovel") {
+            println!("You dig yourself out into another room");
+            input = String::new();
+            break;
         }
     }
-    println!("You put the axe in your inventory");
-    inventory.push("Axe");
+    loop {
+        println!("{}", room2.Description);
+        if input.is_empty() == true {
+            input = get_input();
+        }
+
+        if input.contains("axe") {
+            println!("You grab the axe!");
+            player.Inventory.push(String::from("Axe"));
+            input = String::new();
+        }
+        if input.contains("open") && input.contains("door") {
+            println!("You open the door and walk in.");
+            input = String::new();
+            break;
+        }
+    }
+    println!("broke out of room 2 loop succesfully");
 }
